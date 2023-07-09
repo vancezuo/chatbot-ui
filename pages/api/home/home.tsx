99cdf915345bb33a9e436f89fd4faf5d2dae8d2c
rogerzhuang <87434722+rogerzhuang@users.noreply.github.com>
2023-07-09 12:09:15 -0700

    minor fix on ui

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -392,8 +392,11 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
-        <meta name="description" content="ChatGPT but better." />
+        <title>OmniChat</title>
+        <meta
+          name="description"
+          content="Your Passport to Information Exploration."
+        />
         <meta
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
