6648ebe14cd0c452ae27a8424d7f6b4237acd3e9
Vance Zuo <vance.zuo@gmail.com>
2023-07-20 01:57:24 -0700

    Update _app.tsx basic password flow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
+import { useEffect, useState } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
@@ -11,7 +11,47 @@ const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      let auth = sessionStorage.getItem("auth");
+
+      if (auth === "true") {
+        setIsAuthenticated(true);
+        return;
+      }
+
+      while (auth !== "true") {
+        let pass = prompt("Enter password");
+
+        if (pass === null) { // User clicked cancel
+          setIsAuthenticated(false);
+          return;
+        }
+        else if (pass === process.env.NEXT_PUBLIC_PASSWORD) {
+          sessionStorage.setItem("auth", "true");
+          auth = sessionStorage.getItem("auth");
+        } else {
+          alert("Wrong password");
+        }
+      }
+
+      setIsAuthenticated(true);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>; // Loading state
+  }
+
+  if (!isAuthenticated) {
+    return <div>Access denied</div>; // If authentication failed
+  }
+
+  // If authentication passed
   return (
     <div className={inter.className}>
       <Toaster />
